e033bf6839145efa81abb6447f1ddbd6a0684dc2
Daniel Magnus <dannimagn@gmail.com>
2025-03-27 20:11:33 +0100

    Problem 1 and 2 solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
 
+const repeatString = function(string, num) {
+    let result = []
+    if (num < 0) {
+        return 'ERROR'
+    } else {
+    for (let start = 0; start < num; start++) {
+        result.push(string)
+
+    }
+    return result.join('')
+}
 };
+
+
+repeatString("hey", 10)
 
 // Do not edit below this line
 module.exports = repeatString;
