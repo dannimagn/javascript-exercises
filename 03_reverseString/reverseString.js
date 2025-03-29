83ccf26e5d5d21d9a0be8b585a1e82cb41205406
Daniel Magnus <dannimagn@gmail.com>
2025-03-29 21:37:02 +0100

    Reverse String solved, removeFromArray only 1st test solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,26 @@
-const reverseString = function() {
+/*
+I enter a string
+The script takes the length of that string
+The loop starts at the last indexed number of the string
+The loop then adds the last indexed string to an empty array
+This loops until index number reaches 0
+The end then returns the new variable
+*/
 
+
+
+const reverseString = function(string) {
+    let newString = ['']
+
+    for (let strLength = string.length; strLength > -1; strLength--) {
+        newString.push(string[strLength])
+    }
+    return newString.join('')
 };
+
+reverseString('hello')
+
 
 // Do not edit below this line
 module.exports = reverseString;
+3
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+/* 
+I need to be able to remove multiple values from the array
+
+I already remove a variable from the array with the indexOf method
+
+*/
+
+
+const removeFromArray = function(myArray, num) {
+    const index = myArray.indexOf(num);    
+    console.log(index)
 };
 
+
+removeFromArray([1, 2, 3, 3, 4], 3)
 // Do not edit below this line
 module.exports = removeFromArray;
