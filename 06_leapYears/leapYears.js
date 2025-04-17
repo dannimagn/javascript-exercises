b780c9bfa7c57da85d1412bb5ff907c98f4670e6
Daniel Magnus <dannimagn@gmail.com>
2025-04-17 17:42:59 +0200

    Finished tasks 1-7

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,31 @@
-const leapYears = function() {
+/*
 
-};
+If num % 4 = 0 then we have leap year
+
+but if num % 100 = 0 then no leap year
+
+but also if num % 400 = 0 then leap year
+
+so the order to possibly check is:
+
+if num % 100 = 0  return no leap
+
+else if num % 4 && num % 400 = return leap
+
+*/
+
+
+const leapYears = function(num) {
+    if (num % 4 == 0 && num % 100 == 0 && num % 400 == 0) {
+        return true
+    } else if (num % 4 == 0 && num % 100 != 0) {
+        return true
+    } else {
+        return false
+    }
+}
+
+leapYears(1984)
 
 // Do not edit below this line
 module.exports = leapYears;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+  const conversion = ((value - 32) * (5/9))
+  return parseFloat(conversion.toFixed(1))
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+  const conversion = (value * (9/5) + 32)
+  return parseFloat(conversion.toFixed(1))
 };
 
 // Do not edit below this line
